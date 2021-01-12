d63f56fef5fa60d2592b86906c7a5ecf03763512
JJ <jaroslaw.janikowski@gmail.com>
2021-01-13 00:39:03 +0100

    Poprawienie dodawania tych samych URL.

--- links/models.py
+++ links/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Link(models.Model):
-    long_url = models.URLField(verbose_name='URL', unique=True)
+    long_url = models.URLField(verbose_name='URL')  # kontrola dodawania wielu takich samych URL dokonywana jest podczas zapisu
     date_add = models.DateField(auto_now_add=True)
