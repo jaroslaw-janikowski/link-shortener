e2e2967711e8a78a8be87596be918846fa23c71b
JJ <jaroslaw.janikowski@gmail.com>
2021-01-13 00:19:10 +0100

    Poprawienie bazy.

--- links/models.py
+++ links/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
 class Link(models.Model):
-    short_url = models.CharField(max_length=512, unique=True)
-    long_url = models.TextField(verbose_name='URL', unique=True)
+    long_url = models.URLField(verbose_name='URL', unique=True)
     date_add = models.DateField(auto_now_add=True)
