3d7150523483d5322665924559e670aa13e7b9a4
JJ <jaroslaw.janikowski@gmail.com>
2021-01-12 22:43:55 +0100

    Stylowanie formularza linka.

--- links/models.py
+++ links/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Link(models.Model):
     short_url = models.CharField(max_length=512, unique=True)
-    long_url = models.TextField(unique=True)
+    long_url = models.TextField(verbose_name='URL', unique=True)
     date_add = models.DateField(auto_now_add=True)
--- linkshortener/settings.py
+++ linkshortener/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'home',
     'users',
     'links',
@@ -125,3 +126,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = (
     BASE_DIR / 'node_modules',
 )
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
