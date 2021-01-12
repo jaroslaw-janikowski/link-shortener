20b14a8876310dd74168e325d1ab36d9dc6cd046
JJ <jaroslaw.janikowski@gmail.com>
2021-01-13 00:19:45 +0100

    Dodanie wiadomości dla usera i przekierowań dla krótkich linków.

--- links/urls.py
+++ links/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from links.views import LinkView
+from links.views import LinkView, RedirView
 
 
 urlpatterns = [
     path('', LinkView.as_view(), name='link-add-view'),
+    path(r'<int:pk>/', RedirView, name='link-redirect-view')
 ]
--- links/views.py
+++ links/views.py
@@ -1,9 +1,37 @@
 from django.urls import reverse_lazy
 from django.views.generic import FormView
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from links.forms import LinkForm
+from links.models import Link
 
 
 class LinkView(FormView):
     form_class = LinkForm
     template_name = 'link-add.html'
-    success_url = reverse_lazy('links:link-add_view')
+    success_url = reverse_lazy('links:link-add-view')
+
+    def form_valid(self, form):
+        obj = None
+
+        try:
+            obj = Link.objects.get(
+                long_url=form.cleaned_data['long_url']
+            )
+        except Link.DoesNotExist:
+            obj = Link(long_url=form.cleaned_data['long_url'])
+            obj.save()
+
+        short_url = f'{self.request.scheme}://{self.request.get_host()}/{obj.id}'
+        messages.add_message(self.request, messages.INFO, f'Twój krótki link to: <a target="_blank" href="{short_url}">{short_url}</a>')
+
+        return super().form_valid(form)
+
+
+def RedirView(request, pk, *args):
+    try:
+        p = Link.objects.get(pk=pk)
+    except Link.DoesNotExist:
+        raise Http404('URL nie istnieje.')
+
+    return HttpResponseRedirect(p.long_url)
